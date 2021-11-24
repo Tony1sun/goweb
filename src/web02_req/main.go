ee0ea9aff0b629237dfdaf5f5ebada157c643849
Tony1sun <jacktony822@gmail.com>
2021-11-25 00:05:29 +0800

    add

--- src/web02_req/main.go
+++ src/web02_req/main.go
@@ -13,13 +13,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "请求头中Accept-Encoding的信息有:", r.Header["Accept-Encoding"])
 	fmt.Fprintln(w, "请求头中Accept-Encoding的属性值有:", r.Header.Get("Accept-Encoding"))
 	// 获取请求体内容的长度
-	len := r.ContentLength
-	// 创建byte切片
-	body := make([]byte, len)
-	// 将请求体中的内容读到body中
-	r.Body.Read(body)
-	// 在浏览器中显示请求体中的内容
-	fmt.Fprintln(w, "请求体中的内容有:", string(body))
+	// len := r.ContentLength
+	// // 创建byte切片
+	// body := make([]byte, len)
+	// // 将请求体中的内容读到body中
+	// r.Body.Read(body)
+	// // 在浏览器中显示请求体中的内容
+	// fmt.Fprintln(w, "请求体中的内容有:", string(body))
+
+	// 解析表单,在调用r.Form之前必须执行该操作
+	r.ParseForm()
+	// 获取请求参数
+	fmt.Fprintln(w, "请求参数有:", r.Form)
+	fmt.Fprintln(w, "POST请求的form表单中的请求参数有:", r.PostForm)
 }
 
 func main() {
