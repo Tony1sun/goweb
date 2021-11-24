6ea55a474ea91a5a178cbae4a9666ed31952a710
Tony1sun <jacktony822@gmail.com>
2021-11-24 21:30:06 +0800

    add

--- src/web01_db/model/user.go
+++ src/web01_db/model/user.go
@@ -1,6 +1,9 @@
 package model
 
-import "web01_db/utils"
+import (
+	"fmt"
+	"goweb/utils"
+)
 
 // User结构体
 type User struct {
@@ -10,8 +13,34 @@ type User struct {
 	Email    string
 }
 
-// 添加User的方法
+// 添加User的方法一
 func (user *User) AddUser() error {
 	sqlStr := "insert into users(username, password, email) values(?,?,?)"
 
+	// 预编译
+	inStmt, err := utils.Db.Prepare(sqlStr)
+	if err != nil {
+		fmt.Println("预编译异常:", err)
+		return err
+	}
+	// 执行
+	_, err2 := inStmt.Exec("admin", "123456", "admin@atguigu.com")
+	if err2 != nil {
+		fmt.Println("执行异常:", err2)
+		return err
+	}
+	return nil
+}
+
+// 添加User的方法二
+func (user *User) AddUser2() error {
+	sqlStr := "insert into users(username, password, email) values(?,?,?)"
+
+	// 执行
+	_, err := utils.Db.Exec(sqlStr, "admin2", "6766666", "admin2@sina.com")
+	if err != nil {
+		fmt.Println("执行异常:")
+		return err
+	}
+	return nil
 }
