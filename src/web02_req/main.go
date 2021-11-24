df3dced84f9e96070d9a69a77bcb5efe19d2965c
Tony1sun <jacktony822@gmail.com>
2021-11-24 23:02:40 +0800

    add

---
+++ src/web02_req/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 处理器函数
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "发送的请求的请求地址是:", r.URL.Path)
+	fmt.Fprintln(w, "发送的请求的请求地址后的查询字符串是:", r.URL.RawQuery)
+	fmt.Fprintln(w, "请求头中的所有信息有:", r.Header)
+	fmt.Fprintln(w, "请求头中Accept-Encoding的信息有:", r.Header["Accept-Encoding"])
+	fmt.Fprintln(w, "请求头中Accept-Encoding的属性值有:", r.Header.Get("Accept-Encoding"))
+	// 获取请求体内容的长度
+	len := r.ContentLength
+	// 创建byte切片
+	body := make([]byte, len)
+	// 将请求体中的内容读到body中
+	r.Body.Read(body)
+	// 在浏览器中显示请求体中的内容
+	fmt.Fprintln(w, "请求体中的内容有:", string(body))
+}
+
+func main() {
+	http.HandleFunc("/hello", handler)
+
+	http.ListenAndServe(":8080", nil)
+}
