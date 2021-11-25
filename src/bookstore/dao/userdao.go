fe03ddd83341d8fbc064b952dd1459621ac5dd35
Tony1sun <jacktony822@gmail.com>
2021-11-25 18:16:00 +0800

    add

--- src/bookstore/dao/userdao.go
+++ src/bookstore/dao/userdao.go
@@ -5,12 +5,25 @@ import (
 	"bookstore/utils"
 )
 
-
 func CheckUserNameAndPassword(username string, password string) (*model.User, error) {
 
 	sqlStr := "select id,username,password,email from users where username = ? and pasword = ?"
 
 	row := utils.Db.QueryRow(sqlStr, username, password)
+	
 
+	user := &model.User{}
+	row.Scan(user.ID, user.Username, user.Password, user.Email)
+	return user, nil
+}
 
+func CheckUserName(username string) (bool, error) {
+
+	sqlStr := "select id,username,password,email from users where username = ?"
+
+	row := utils.Db.QueryRow(sqlStr, username)
+
+	user := &model.User{}
+	row.Scan(user.ID, user.Username, user.Password, user.Email)
+	return user, nil
 }
