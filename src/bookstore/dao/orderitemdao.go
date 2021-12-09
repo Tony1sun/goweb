ae90c4b40b03684d29b112f58af57b3dd4a7f67a
Tony1sun <jacktony822@gmail.com>
2021-12-09 14:39:25 +0800

    add

--- src/bookstore/dao/orderitemdao.go
+++ src/bookstore/dao/orderitemdao.go
@@ -9,7 +9,7 @@ import (
 func AddOrderItem(orderItem *model.OrderItem) error {
 	sql := "insert into order_items(count, amount, title, author, price, img_path, order_id) values(?,?,?,?,?,?,?)"
 	// 执行
-	_, err := utils.Db.Exec(sql, orderItem.Count, orderItem.Amount, orderItem.Title, orderItem.Author, orderItem.Price, orderItem.Price, orderItem.ImgPath, orderItem.OrderID)
+	_, err := utils.Db.Exec(sql, orderItem.Count, orderItem.Amount, orderItem.Title, orderItem.Author, orderItem.Price, orderItem.ImgPath, orderItem.OrderID)
 	if err != nil {
 		return err
 	}
